a255ba403d531833e3b91772bced7ecad265aa87
keorus <keorus@protonmail.com>
2020-10-25 05:27:19 -0400

    First Version

--- main.py
+++ main.py
@@ -1,75 +1,71 @@
-#!//usr/bin/env python3
-# coding:utf8
-import string
-import sys
+#!/usr/bin/env python3
+# coding:utf-8
 import time
-import hashlib
 import argparse
+import atexit
+from cracker import *
+import multiprocessing
 
 
+def duration_poster():
+    """
+    Shows the duration of the program
+    :return:
+    """
 
-def crack_dict(md5, file):
-    try:
-        trouve = False
-        ofile = open(file, "r")
-        for mot in ofile.readlines():
-            mot = mot.strip("\n")
-            hashmd5 = hashlib.md5(mot.encode("utf8")).hexdigest()
-            if hashmd5 == md5:
-                print("Mot de passe trouvé : " + str(mot) + " (" + hashmd5 + ")")
-                trouve = True
-        if not trouve:
-            print("Mot de passe non trouvé :(")
-        ofile.close()
-    except FileNotFoundError:
-        print("Erreur ; nom de dossier ou fichier introuvable !")
-        sys.exit(1)
-    except Exception as err:
-        print("Erreur : " + str(err))
-        sys.exit(2)
+    print("Elapsed time : " + str(time.time() - start) + " seconds")
 
 
-def crack_incr(md5, length, currpass=[]):
-    lettres = string.ascii_letters
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Python password breaker")
+    parser.add_argument("-f", "--file", dest="file", help="Keyword file path", required=False)
+    parser.add_argument("-g", "--gen", dest="gen", help="Generate an MD5 hash of the given password", required=False)
+    parser.add_argument("-md5", dest="md5", help="MD5 password to break", required=False)
+    parser.add_argument("-l", dest="plength", help="Password length (incremental mode only)",
+                        required=False, type=int)
+    parser.add_argument("-o", dest="online", help="Search the hash online (google)", required=False,
+                        action="store_true")
+    args = parser.parse_args()
 
-    if length >= 1:
-        if len(currpass) == 0:
-            currpass = ['a' for _ in range(length)]
-            crack_incr(md5, length, currpass)
+    work_queue = multiprocessing.Queue()
+    done_queue = multiprocessing.Queue()
+    cracker = Cracker()
+    start = time.time()
+    atexit.register(duration_poster)
+
+    if args.gen:
+        print("[*] HASH MD5 TO " + args.gen + " = " + hashlib.md5(args.gen.encode("utf8")).hexdigest())
+
+    if args.md5:
+        print("[*] HASH CRACKING " + args.md5)
+        if args.file:
+            print("[*] USING THE KEYWORDS FILE " + args.file)
+
+            p1 = multiprocessing.Process(target=Cracker.work, args=(work_queue, done_queue, args.md5, args.file,
+                                                                    Order.DESCEND))
+            work_queue.put(cracker)
+            p1.start()
+
+            p2 = multiprocessing.Process(target=Cracker.work, args=(work_queue, done_queue, args.md5, args.file,
+                                                                    Order.ASCEND))
+            work_queue.put(cracker)
+            p2.start()
+
+            while True:
+                data = done_queue.get()
+
+                if data == "FIND" or data == "NOT FOUND":
+                    p1.kill()
+                    p2.kill()
+                    break
+
+        elif args.plength:
+            print("[*] USING INCREMENTAL MODE TO " + str(args.plength) + " LETTER(S)")
+            Cracker.crack_incr(args.md5, args.plength)
+        elif args.online:
+            print("[*] USING ONLINE MODE")
+            Cracker.crack_online(args.md5)
         else:
-            for c in lettres:
-                currpass[length - 1] = c
-                print("Trying : " + "".join(currpass))
-                if hashlib.md5("".join(currpass).encode("utf8")).hexdigest() == md5:
-                    print("PASSWORD FOUND! " + "".join(currpass))
-                else:
-                    crack_incr(md5, length - 1, currpass)
-
-
-parser = argparse.ArgumentParser(description="Password Cracker")
-parser.add_argument("-f", "--file", dest="file", help="Path of the dictionary file", required=False)
-parser.add_argument("-g", "--gen", dest="gen", help="Generate MD5 hash of password", required=False)
-parser.add_argument("-md5", dest="md5", help="Gashed password (MD5)", required=False)
-parser.add_argument("-l", dest="plength", help="Password length", required=False, type=int)
-
-args = parser.parse_args()
-
-debut = time.time()
-
-if args.md5:
-    print("[CRACKING HASH " + args.md5 + "]")
-    if args.file:
-        print("[USING DICTIONARY FILE " + args.file + "]")
-        crack_dict(args.md5, args.file)
-    elif args.plength:
-        print("[USING INCREMENTAL MOD FOR " + str(args.plength) + " letter(s)")
-        crack_incr(args.md5, args.plength)
+            print(Color.RED + "[-] PLEASE CHOOSE THE ARGUMENT -f OR -l with -md5." + Color.END)
     else:
-        print("Please choose either -f or -l argument")
-else:
-    print("MD5 HASH NOT PROVIDED")
-
-if args.gen:
-    print("[MD5 HASH OF " + args.gen + " : " + hashlib.md5(args.gen.encode("utf8")).hexdigest())
-
-print("Durée : " + str(time.time() - debut) + " secondes")
+        print(Color.RED + "[-] HASH MD5 NOT PROVIDED." + Color.END)
